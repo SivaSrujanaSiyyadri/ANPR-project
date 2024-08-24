3becae73241a457a88175c112c8e583ea97447cf
SivaSrujanaSiyyadri <161793302+SivaSrujanaSiyyadri@users.noreply.github.com>
2024-08-24 11:33:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 echo "[general]
-email = \"ralhanprateek@gmail.com\"
+email = \"siyyadrisivasrujana@gmail.com\"
 " > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
